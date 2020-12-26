059c297f4e277cb0c573538e48e0d60f122a74bc
Maximilian Dazer <maximilian.dazer@gmx.de>
2020-12-26 21:39:05 +0000

    added sources for some code

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,3 +1,5 @@
+// Javascript code for navigation bar from: https://www.w3schools.com/howto/howto_js_topnav_responsive.asp
+
 function responsiveNav(){
     var x = document.getElementById("navbar");
     if(x.className === "navigation"){

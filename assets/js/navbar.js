b19728fb9a0a3abb9adedebba11970fe3dffcc74
Maximilian Dazer <maximilian.dazer@gmx.de>
2020-12-27 01:21:26 +0000

    changed navigation bar html structure and css rules, navigation bar is now responsive

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,9 +1,13 @@
-// Javascript code for navigation bar from: https://www.w3schools.com/howto/howto_js_topnav_responsive.asp
+
+const burgerDropdown = document.querySelector(".dropdown"); // Javascript code for navigation bar from: https://www.w3schools.com/howto/howto_js_topnav_responsive.asp
+const navBar = document.querySelector(".navigation");
 
 function responsiveNav(){
-    var x = document.getElementById("navbar");
-    if(x.className === "navigation"){
-        x.className += " responsive";
-    }
-    else{x.className = "navigation"}
+    burgerDropdown.classList.add("disabled");
+    navBar.classList.add("responsive")
+}
+
+function closeNav() {
+     burgerDropdown.classList.remove("disabled");
+     navBar.classList.remove("responsive")
 }
